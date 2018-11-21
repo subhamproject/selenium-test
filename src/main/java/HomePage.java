68e94f0e8cd1c70bb7480932f613b2521cb404f7
subhamproject <subhammandal01@gmail.com>
2018-11-21 13:02:09 +0530

    Update HomePage.java

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomePage {
     public static void main(String[] args){
-        System.setProperty("webdriver.chrome.driver", "D:\\sahaja\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Subham\\Downloads\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.ultimateqa.com/automation/");
